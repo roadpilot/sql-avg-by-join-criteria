1a72df25373c4d3bd4b26ed7be88954a3fa02949
Martin Achtermann <65299501+roadpilot@users.noreply.github.com>
2021-10-06 19:16:35 +0000

    problem

---
+++ main.sql
@@ -0,0 +1,5 @@
+/*
+Given the CITY and COUNTRY tables, query the names of all the continents (COUNTRY.Continent) and their respective average city populations (CITY.Population) rounded down to the nearest integer.
+
+Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
+*/
